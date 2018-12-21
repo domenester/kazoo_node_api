35817b84bb43ce6737f53c135daa09e4801f6ef0
Marcus Leal <mvleal@interaxa.com>
2018-12-21 11:33:27 -0200

    includ path recover password in public paths

--- src/config/server.ts
+++ src/config/server.ts
@@ -9,7 +9,8 @@ export default {
     avatar: UploadProfilePicturePath
   },
   publicPath: [
-    { path: "/login", method: ["POST"] }
+    { path: "/login", method: ["POST"] },
+    { path: "/password/request", method: ["POST"] }
   ],
   httpsKey: "/etc/letsencrypt/live/epicall.interaxa.com/privkey.pem",
   httpsCert: "/etc/letsencrypt/live/epicall.interaxa.com/cert.pem"
